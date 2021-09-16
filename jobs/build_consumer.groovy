87af444e6c24fa106c43ae22b9880c7ad1d4c3fd
Viet Nguyen <me@tuanviet.ru>
2021-09-16 07:18:58 +0300

    feat: update Jenkins config

--- jobs/build_consumer.groovy
+++ jobs/build_consumer.groovy
@@ -1,6 +1,27 @@
 job('pact-consumer-demo-build') {
   scm {
-    git('https://github.com/qadojo/pact-demo-consumer/')
+    git {
+      remote {
+        github('qadojo/pact-demo-consumer')
+        refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+      }
+      branch('${sha1}')
+    }
+  }
+  triggers {
+    githubPullRequest {
+      admin('github-bot-qadojo')
+      useGitHubHooks()
+      extensions {
+        commitStatus {
+          context('QADojo demo Jenkins')
+          statusUrl('https://jenkins.pact-demo.qadojo.ru/')
+          completedStatus('SUCCESS', 'Build succeed')
+          completedStatus('FAILURE', 'Build failed')
+          completedStatus('ERROR', 'Build failed')
+        }
+      }
+    }
   }
   steps {
     shell('npm i')
